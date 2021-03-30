b6fd318652743613cdc2ee8e5e2647a6f75ffe13
JuiMin <d95wang@gmail.com>
2021-03-30 00:49:12 -0700

    Adjust cpu_settings and rename
    Renaming because there might be other system related stuff here and it
    would be convenient to use the same file.
    
    This also rips out the support for 32 bit systems and mips architectures
    because we are a small team and we currently don't want to over
    complicate things. Additionally pointer business in Crystal is a bit
    tricky so locking ourselves to only 64 bit pointers will simplify things
    until later.

--- src/system_settings.cr
+++ src/system_settings.cr
@@ -3,16 +3,11 @@
 
 # Listing supported CPU types
 
-# 32 bit platforms
-# - 386
-# - ARM
-# - mipsx
-# - ppc
-
 # 64 bit platforms
 # - AMD64 (x86_64)
 # - ARM
-# - mips64x
+
+# In theory these should also work but we haven't tested them
 # - riscv64
 # - ppc64
 # - ppc64le
@@ -25,26 +20,8 @@
 # MAX_MAP_SIZE represents the largest mmap size supported by Levin.
 # MAX_ALLOC_SIZE is the size used when creating array pointers.
 
-# Generic 32 bits
-lib CPU_SETTINGS_32
-    MAX_MAP_SIZE = 0xFFFFFFFFFFFF # 256TB
-    MAX_ALLOC_SIZE = 0x7FFFFFFF
-end
-
 # Generic 64 bits
 lib CPU_SETTINGS_64
     MAX_MAP_SIZE = 0x7FFFFFFF # 2GB
     MAX_ALLOC_SIZE = 0xFFFFFFF
-end
-
-# special settings for less generic CPUs
-# mips
-lib CPU_SETTINGS_MIPS_32
-    MAX_MAP_SIZE = 0x40000000 # 1GB
-    MAX_ALLOC_SIZE = 0xFFFFFFF
-end
-
-lib CPU_SETTINGS_MIPS_64
-    MAX_MAP_SIZE = 0x8000000000 # 512GB
-    MAX_ALLOC_SIZE = 0x7FFFFFFF
 end
